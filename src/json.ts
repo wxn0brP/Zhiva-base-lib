9b2d77c6ce465d400da11fcb1bff806c70a53636
wxn0brP <toja.filiper@gmail.com>
2025-12-16 23:33:31 +0100

    feat: loadJson types

--- src/json.ts
+++ src/json.ts
@@ -1,6 +1,6 @@
 import { existsSync, readFileSync } from "fs";
 
-export function loadJson(path = "package.json", def = {}) {
+export function loadJson<T = Record<string, any>>(path = "package.json", def = {} as T) {
     if (!existsSync(path)) return def;
     return JSON.parse(readFileSync(path, "utf-8"));
 }
