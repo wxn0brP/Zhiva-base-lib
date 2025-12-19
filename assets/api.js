49b90041e3b540e6398908509b47cce9c0e8ab72
Vivienne Valtheris <toja.filiper@gmail.com>
2025-12-19 13:34:20 +0100

    feat: assets/api - get params util

--- assets/api.js
+++ assets/api.js
@@ -2,8 +2,14 @@
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("secret");
 
-    window.fetchApi = (url, opts) => {
-        return fetch(url, {
+    window.fetchApi = (url, opts = {}, query = {}) => {
+        const urlObj = new URL(url, window.location.origin);
+
+        Object.entries(query).forEach(([key, value]) => {
+            urlObj.searchParams.set(key, value);
+        });
+
+        return fetch(urlObj.toString(), {
             ...opts,
             headers: {
                 ...opts.headers,
