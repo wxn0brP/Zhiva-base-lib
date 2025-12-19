c57f33f56bd61053de11f7c8408c16a014394462
wxn0brP <toja.filiper@gmail.com>
2025-12-19 20:12:08 +0100

    feat: fetchApi ts

---
+++ src/front/api.ts
@@ -0,0 +1,20 @@
+import { ZhivaApiOptions, ZhivaApiQuery } from "../types/api";
+
+const urlParams = new URLSearchParams(window.location.search);
+const token = urlParams.get("secret");
+
+export function fetchApi(url: string, opts: ZhivaApiOptions = {}, query: ZhivaApiQuery = {}) {
+    const urlObj = new URL(url, window.location.origin);
+
+    Object.entries(query).forEach(([key, value]) => {
+        urlObj.searchParams.set(key, value.toString());
+    });
+
+    return fetch(urlObj.toString(), {
+        ...opts,
+        headers: {
+            ...opts.headers,
+            "X-Zhiva-Token": token
+        }
+    });
+};
