e78c1d227aafc46eb498ba22f8b5af76ab591dbe
wxn0brP <toja.filiper@gmail.com>
2026-01-10 20:57:20 +0100

    feat: improve fetchApi

--- src/front/api.ts
+++ src/front/api.ts
@@ -4,7 +4,8 @@ const urlParams = new URLSearchParams(window.location.search);
 const token = urlParams.get("secret");
 
 export function fetchApi(url: string, opts: ZhivaApiOptions = {}, query: ZhivaApiQuery = {}) {
-    const urlObj = new URL("/api/" + url, window.location.origin);
+    if (!url.startsWith("/")) url = "/" + url;
+    const urlObj = new URL("/api" + url, window.location.origin);
 
     Object.entries(query).forEach(([key, value]) => {
         urlObj.searchParams.set(key, value.toString());
@@ -18,3 +19,19 @@ export function fetchApi(url: string, opts: ZhivaApiOptions = {}, query: ZhivaAp
         }
     });
 };
+
+export async function fetchApiPost(url: string, body: Record<string, any> = {}, opts: ZhivaApiOptions = {}, query: ZhivaApiQuery = {}) {
+    return await fetchApi(url, {
+        method: "POST",
+        ...opts,
+        body: JSON.stringify(body),
+        headers: {
+            ...opts.headers,
+            "Content-Type": "application/json"
+        }
+    }, query).then(res => res.json());
+}
+
+export async function fetchApiJson(url: string, opts: ZhivaApiOptions = {}, query: ZhivaApiQuery = {}) {
+    return await fetchApi(url, opts, query).then(res => res.json());
+}
