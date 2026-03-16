f07f3e3c8a22214b2109df883e014011ff2e4d0c
wxn0brP <toja.filiper@gmail.com>
2026-03-16 19:57:07 +0100

    fix: open window

--- src/openWindow.ts
+++ src/openWindow.ts
@@ -2,8 +2,17 @@ import { spawn } from "child_process";
 import { homedir } from "os";
 import { join, resolve } from "path";
 
-let zhiva = process.env.ZHIVA_ENGINE || join(homedir(), ".zhiva/zhiva");
-if (process.platform === "win32") zhiva += ".exe";
+let zhiva: string;
+
+if (process.env.ZHIVA_ENGINE)
+    zhiva = process.env.ZHIVA_ENGINE;
+else if (process.env.ZHIVA_ROOT)
+    zhiva = join(process.env.ZHIVA_ROOT, "zhiva");
+else
+    zhiva = join(homedir(), ".zhiva", "zhiva");
+
+if (process.platform === "win32" && !zhiva.toLowerCase().endsWith(".exe"))
+    zhiva += ".exe";
 
 export function openWindow(url: string | number, title?: string) {
     const proc = spawn(
