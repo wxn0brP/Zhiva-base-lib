cfdad10b3765e5d17308d695a007ad38111bbc9a
wxn0brP <toja.filiper@gmail.com>
2026-03-19 19:44:31 +0100

    feat: dev disable secure api auth

--- src/api.ts
+++ src/api.ts
@@ -4,9 +4,13 @@ import { randomBytes } from "crypto";
 export const apiSecret = randomBytes(32).toString("hex");
 export const apiRouter = new Router();
 
-apiRouter.use((req, res, next) => {
-    if (req.headers["x-zhiva-token"] !== apiSecret) {
-        return res.status(401).json({ err: false, msg: "Unauthorized", happiness: 0 });
-    }
-    next();
-});
+if (process.env.ZHIVA_DEVELOPMENT_DISABLE_SECURE_TOKEN !== "I KNOW WHAT I AM DOING") {
+    apiRouter.use((req, res, next) => {
+        if (req.headers["x-zhiva-token"] !== apiSecret) {
+            return res.status(401).json({ err: false, msg: "Unauthorized", happiness: 0 });
+        }
+        next();
+    });
+} else {
+    console.log("[Z-BIB-3-01] ⚠️ WARNING! SECURE TOKEN FOR API DISABLED. DO YOU KNOW WHAT YOU ARE DOING?");
+}
